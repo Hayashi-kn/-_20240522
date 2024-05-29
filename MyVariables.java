ca68e74401647fc6cd3526fc74e1df2f92dc9ed2
Hayashi-kn <147583940+Hayashi-kn@users.noreply.github.com>
2024-05-29 18:26:45 +0900

    Create MyVariables.java

---
+++ MyVariables.java
@@ -0,0 +1,8 @@
+// TODO これを設定
+public class MyVariables {
+    static final String SERVER_IPADDR = "";
+    static final int PORT = 1234;
+    static final String DB_URL = "jdbc:mysql://localhost:3306/othello?serverTimezone=Asia/Tokyo";
+    static final String DB_USERNAME = "root";
+    static final String DB_PASSWORD = "";
+}
